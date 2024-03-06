31cff98f557e180a93a94830787bb5259eef0978
gsreepalvh <psreepalv@gmail.com>
2024-03-07 00:38:45 +0530

    meaningful name

--- svg-js/svg.js
+++ svg-js/svg.js
@@ -5,50 +5,52 @@ However, SVG elements cannot be dynamically created using createElement in the s
 https://webhint.io/docs/user-guide/hints/hint-create-element-svg/#:~:text=However%2C%20SVG%20elements%20cannot%20be,instead%20of%20an%20SVG%20element.
 */
 
-const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
 const width = window.innerWidth;
 const height = window.innerHeight;
-const n = 100;
+
+const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg.setAttribute('width', width);
 svg.setAttribute('height', height);
 document.body.appendChild(svg);
 
-const mask = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
-mask.setAttribute('id', 'mask-circle');
-svg.appendChild(mask);
+const mask_circle_y = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
+mask_circle_y.setAttribute('id', 'mask-circle-y');
+svg.appendChild(mask_circle_y);
 
-const mask2 = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
-mask2.setAttribute('id', 'mask-circle-2');
-svg.appendChild(mask2);
+const mask_circle_x = document.createElementNS('http://www.w3.org/2000/svg', 'mask');
+mask_circle_x.setAttribute('id', 'mask-circle-x');
+svg.appendChild(mask_circle_x);
 
-const maskRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+const mask_rect_y = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
 
-    maskRect.setAttribute('width', width);
-    maskRect.setAttribute('height', height);
-    maskRect.setAttribute('fill', 'black');
-    mask.appendChild(maskRect);
+mask_rect_y.setAttribute('width', width);
+mask_rect_y.setAttribute('height', height);
+mask_rect_y.setAttribute('fill', 'black');
+    mask_circle_y.appendChild(mask_rect_y);
     
-    const maskRect2 = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    const mask_rect_x = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
     
-        maskRect2.setAttribute('width', width);
-        maskRect2.setAttribute('height', height);
-        maskRect2.setAttribute('fill', 'white');
-        mask2.appendChild(maskRect2);
+    mask_rect_x.setAttribute('width', width);
+    mask_rect_x.setAttribute('height', height);
+    mask_rect_x.setAttribute('fill', 'white');
+    mask_circle_x.appendChild(mask_rect_x);
 
-const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-circle.setAttribute('cx', width/2);
-circle.setAttribute('cy', height/2);
-circle.setAttribute('r', 200);
-circle.setAttribute('fill', 'white');
-mask.appendChild(circle);
+const circle_y = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+circle_y.setAttribute('cx', width/2);
+circle_y.setAttribute('cy', height/2);
+circle_y.setAttribute('r', 200);
+circle_y.setAttribute('fill', 'white');
+mask_circle_y.appendChild(circle_y);
 
-const circle2 = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-circle2.setAttribute('cx', width/2);
-circle2.setAttribute('cy', height/2);
-circle2.setAttribute('r', 200);
-circle2.setAttribute('fill', 'black');
-mask2.appendChild(circle2);
+const circle_x = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+circle_x.setAttribute('cx', width/2);
+circle_x.setAttribute('cy', height/2);
+circle_x.setAttribute('r', 200);
+circle_x.setAttribute('fill', 'black');
+mask_circle_x.appendChild(circle_x);
 
+const n = 100;
 for(let i=0;i<n;i++){
 
     const recty = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
@@ -56,7 +58,7 @@ for(let i=0;i<n;i++){
     recty.setAttribute('width', width);
     recty.setAttribute('height', '10');
     
-    recty.setAttribute('mask','url(#mask-circle)');
+    recty.setAttribute('mask','url(#mask-circle-y)');
     svg.appendChild(recty);
     
 }
@@ -67,7 +69,7 @@ for(let i=0;i<n;i++){
     rectx.setAttribute('x', i*20);
     rectx.setAttribute('width', 10);
     rectx.setAttribute('height', height);
-    rectx.setAttribute('mask','url(#mask-circle-2)');
+    rectx.setAttribute('mask','url(#mask-circle-x)');
     svg.appendChild(rectx);
 
 
